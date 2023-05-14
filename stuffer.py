38d52934f56953e3fa63e9665a72d216d6ea6376
HushKante <129730806+HushKante@users.noreply.github.com>
2023-05-14 09:38:12 +0800

    Update stuffer.py

--- stuffer.py
+++ stuffer.py
@@ -1,4 +1,5 @@
 import random,os,sys
+from tqdm import tqdm
 
 def isn(a):
     b=str(a)
@@ -6,16 +7,19 @@ def isn(a):
         b=input("[!]Invaild input.Please retype:")
     return b
 
-print("Siliconioa Stuffer , Version 1.0a")
-print("[!]Warings!This may cause unexcepted damage to your system.")
-nam = input("[*]Enter the filename:")
-len = int(isn(input("[*]Enter the file size:")))
-how = int(isn(input("[*]Select which type of characters to stuff: [1]unreadable [2]readable [3]hybrid:")))
-f=open(nam,"w")
-for i in range(len):
-    f.write(chr(random.randint(32-32*(how%2),31+96*(how//2))))
-f.close()
-print('[*]Stuffed file generated: %s' % os.path.join(os.getcwd(), nam))
-print("[*]File size:"+str(len//1048576)+"MiB(s)"+str((len-1048576*(len//1048576))//1024)+"KiB(s)"+str(len%1024)+"Byte(s)")
+try:
+    
+    print("Siliconioa Stuffer , Version 1.1a")
+    print("[!]Warings!This may cause unexcepted damage to your system.")
+    nam = input("[*]Enter the filename:")
+    f=open(nam,"w")
+    len = int(isn(input("[*]Enter the file size:")))
+    how = int(isn(input("[*]Select which type of characters to stuff: [1]unreadable [2]readable [3]hybrid:")))
+    for i in tqdm(range(len),unit="Byte(s)",unit_scale=True):
+        f.write(chr(random.randint(32-32*(how%2),31+95*(how//2))))
+    f.close()
+    print('[*]Stuffed file generated: %s' % os.path.join(os.getcwd(), nam))
+    print("[*]Added file size:"+str(len//1048576)+"MiB(s)"+str((len-1048576*(len//1048576))//1024)+"KiB(s)"+str(len%1024)+"Byte(s)")
 
-#Written by HushKante ,2023.5.1
+except:
+    print("[!] Fatal error!Please run this script again...")
